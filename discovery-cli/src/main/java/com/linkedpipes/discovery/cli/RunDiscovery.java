c593563f862025c68b0fc855f8cbe3673011ca4f
Petr Škoda <skodapetr@gmail.com>
2020-03-21 12:59:48 +0100

    Fix compulation

--- discovery-cli/src/main/java/com/linkedpipes/discovery/cli/RunDiscovery.java
+++ discovery-cli/src/main/java/com/linkedpipes/discovery/cli/RunDiscovery.java
@@ -28,7 +28,6 @@ import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
